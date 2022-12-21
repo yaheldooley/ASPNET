e6bc5347d0df98a7c98c6f94244e4ceb6a60acd6
yahel dooley <ydooley@gmail.com>
2022-12-20 21:40:32 -0800

    Added delete functionality

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -51,5 +51,11 @@ namespace Testing.Controllers
 			repo.InsertProduct(productToInsert);
 			return RedirectToAction("Index");
 		}
+
+		public IActionResult DeleteProduct(Product product)
+		{
+			repo.DeleteProduct(product);
+			return RedirectToAction("Index");
+		}
 	}
 }
--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -11,5 +11,6 @@ namespace Testing
 		public void InsertProduct(Product productToInsert);
 		public IEnumerable<Category> GetCategories();
 		public Product AssignCategory();
+		public void DeleteProduct(Product product);
 	}
 }
